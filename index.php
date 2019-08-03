870d2b589f99ab3c397b3e4ac78579c640bbf160
Rennan Morais <rennan_vi@hotmail.com>
2019-08-02 21:57:41 -0300

    Inserido os assets e frameworks que serão usados e criado o index da página

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>RM | Web Developer</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="assets/js/script.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
